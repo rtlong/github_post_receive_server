e449486ecf81b6ec8e134f2325600cd87e5ff781
Ryan Taylor Long <ryan@rtlong.com>
2012-02-15 17:08:28 -0600

    Switch to using Gmail for email

--- email.rb
+++ email.rb
@@ -3,22 +3,21 @@ require 'net/smtp'
 
 class Email
   def initialize(opts={})
-    @server     = opts[:server]     || 'home.rtlong.com'
-    @from       = opts[:from]       || 'postbox@rtlong.com'
+    @from       = opts[:from]       || 'github@rtlong.com'
     @from_alias = opts[:from_alias] || 'Git Push Notifier'
     @body       = opts[:body]
     @subject    = opts[:subject]
   end
-  attr_accessor :server, :from, :from_alias, :body, :subject
+  attr_accessor :from, :from_alias, :body, :subject
 
-  def send(to,opts={})    
+  def send(to,opts={})
     instance_variables.each do |var|
       option_name = var[1..-1].to_sym
       opt = opts.delete(option_name)
       instance_variable_set( var, opt ) if opt
     end
     $stderr.puts "Email#send was called with the following unimplemented options:\n #{opts.inspect}" unless opts.empty?
-    
+
     raise ArgumentError, "You need to specify the :body option." unless @body
 
     msg = <<END_OF_MESSAGE
@@ -29,10 +28,12 @@ Subject: #{@subject}
 #{@body}
 END_OF_MESSAGE
 
-    Net::SMTP.start(@server) do |smtp|
+    smtp = Net::SMTP.new('smtp.gmail.com', 587)
+    smtp.enable_starttls
+    smtp.start(ENV['GOOGLE_MAIL_DOMAIN'], ENV['GOOGLE_MAIL_LOGIN'].strip, ENV['GOOGLE_MAIL_PASSWORD'].strip, :login) do
       smtp.send_message msg, @from, to
-      puts "Sent mail (#{@subject || "{blank subject}"}) to #{to}" 
     end
+    puts "Sent mail (#{@subject || "{blank subject}"}) to #{to}"
   end
 end
- 
+
